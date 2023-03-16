f91deb1cbd39c4f8fe04facf1fa7aad3c6d98ef7
Gabriel Vendramini <gabriel_vendramini@hotmail.com>
2023-03-16 15:04:21 +0100

    some refactor for main.py

--- main.py
+++ main.py
@@ -7,13 +7,40 @@ from service.character_service import create_character, character_str
 hero_name = "Gandalf"
 enemy_name = "Sauron"
 
-enemy_attributes = Attributes(10, 10, 10)
-hero_attributes = Attributes(10, 10, 10)
+hero_race = "Elf"
+enemy_race = "Elf"
+
+hero_class = "Wizard"
+enemy_class = "Wizard"
+
+hero_attack = 10
+enemy_attack = 10
+
+hero_defense = 5
+enemy_defense = 5
+
+hero_level = 1
+enemy_level = 1
+
+hero_experience = 0
+enemy_experience = 0
+
+hero_intelligence = 10
+enemy_intelligence = 10
+
+hero_dexterity = 10
+enemy_dexterity = 10
+
+hero_strength = 10
+enemy_strength = 10
+
+enemy_attributes = Attributes(hero_intelligence, hero_dexterity, hero_strength)
+hero_attributes = Attributes(enemy_intelligence, enemy_dexterity, enemy_strength)
 
 
 # Create characters
-hero = create_character(hero_name, 10, 5, 1, hero_attributes, "Elf", "Wizard", race_data, class_data, 0)
-enemy = create_character(enemy_name, 10, 5, 1, enemy_attributes, "Elf", "Wizard", race_data, class_data, 0)
+hero = create_character(hero_name, hero_attack, hero_defense, hero_level, hero_attributes, hero_race, hero_class, race_data, class_data, hero_experience)
+enemy = create_character(enemy_name, enemy_attack, enemy_defense, enemy_level, enemy_attributes, enemy_race, enemy_class, race_data, class_data, enemy_experience)
 
 
 # Print character info
